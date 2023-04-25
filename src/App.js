44a429c48a83f0f693da128e8d66f41c9d537c67
Asude Fışkın <74365269+asuf29@users.noreply.github.com>
2023-04-25 23:48:05 +0300

    ProductList.js created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Nav from "./components/Nav";
 import CategoryList from "./components/CategoryList";
+import ProductList from "./components/ProductList";
 function App() {
   return (
     <div className="App">
       <h3>Hello React</h3>
       <Nav />
       <CategoryList />
+      <ProductList />
     </div>
   );
 }
