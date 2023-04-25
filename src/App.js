4ff3d9f0a0730b8d81ba551db641093a28edc397
Asude Fışkın <74365269+asuf29@users.noreply.github.com>
2023-04-25 23:43:36 +0300

    Nav.js created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
-
+import Nav from "./components/Nav.js";
 function App() {
   return (
     <div className="App">
       <h3>Hello React</h3>
+      <Nav />
     </div>
   );
 }
