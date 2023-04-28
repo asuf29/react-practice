64400ac6ef79642e58413c48900ea3a0cf264cf3
Asude Fışkın <74365269+asuf29@users.noreply.github.com>
2023-04-28 22:38:09 +0300

    I defined state in ProductList component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ProductList(props) {
+  const [selectedCategory, setSelectedCategory] = useState(null);
   return (
     <div>
       <h3>{props.title}</h3>
